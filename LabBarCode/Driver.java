16c675bbae9b619cdbc7a35e5143b823bd9f562b
esandine <esandine@stuy.edu>
2015-12-22 19:40:31 -0500

    Constructor 1 works

---
+++ LabBarCode/Driver.java
@@ -0,0 +1,18 @@
+public class Driver{
+    public static void main(String[] args){
+	try{
+	    BarCode b1 = new BarCode("39495");
+	    System.out.println(b1);
+	    //	    BarCode b2 = new BarCode("00001");
+	    //System.out.println("Works?");
+	    //BarCode b3 = new BarCode("3949");
+	    //System.out.println("Works?");
+	    //BarCode b4 = new BarCode("3949345");
+	    //System.out.println("Works?");
+	    BarCode b5 = new BarCode("3949a");
+	    System.out.println("Works?");
+	}catch(RuntimeException e){
+	    System.out.println("Invlaid bar code");
+	}
+    }
+}
